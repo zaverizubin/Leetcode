335a3e33f1c98585a7f9345f084aca400615421a
zubin zaveri <zaverizubin@gmail.com>
2017-02-11 12:26:38 +0530

    Leetcode first commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        private static Test _myTest;
+
+        static void Main(string[] args)
+        {
+            _myTest = new Test();
+
+            //_myTest.HammingDistance(1,4);
+
+            //_myTest.FindCompliment(5);
+
+            //myTest.ReverseString("abc");
+
+            //_myTest.BestProfit(new List<int>() { 10, 7, 5, 8, 11, 9 });
+
+            //_myTest.GetProductOfAllIntsExceptAtIndex(new int[]{5,1,2,4,3 });
+
+            //_myTest.GetHighestProduct(new List<int>() { 1, 2, 3, 3, 2, 1 });
+
+            //_myTest.GetLargestNonRepeatingSubstring("wzrjugcptmrserlhhoaudcboimpdrpaqqrzmxddtqvluoweymgspitfshwwynwqfnqrnv");
+
+            //_myTest.LongestPalindromicSubstring("xyzlolololabccba");
+
+            //_myTest.IslandInOcean(new[,] {{0, 1, 0, 0}, {1, 1, 1, 1}, {0, 1, 0, 0}, {1, 1, 0, 0}});
+
+            //_myTest.StringRepresentationOfNumbers(15);
+
+            //_myTest.NimGame(5);s
+
+            //_myTest.SingleNumber(new int[] {17,12,5,-6,12,4,17,-5,2,-3,2,4,5,16,-3,-4,15,15,-4,-5,-6});
+
+            //_myTest.GetSum(5, 6);
+
+            //_myTest.AssignCookies(new int[]{1,2,2,2}, new int[]{1,2,1});
+
+            //_myTest.FindTheDifference("ab", "aba");
+
+            //_myTest.AddDigits(12345)
+
+            //_myTest.MaxDepthOfBinaryTree(new TreeNode(10){left = new TreeNode(2){left = new TreeNode(1){left = new TreeNode(0){right = new TreeNode(-1)}},right = new TreeNode(4){left = new TreeNode(3)}},right = new TreeNode(6){left = new TreeNode(5)}});
+
+            //_myTest.MoveZeroes(new[] { 1, 0, 2, 3, 0, 1 });
+
+            //_myTest.ClosestToZero(new int[]{});
+
+            //_myTest.MinMoves(new int[] { });
+
+            //_myTest.RansomNote("aa", "aba");
+
+            //_myTest.IntersectionOfTwoArrays(new[] { 1, 2, 2, 1 }, new int[] { 2, 2 });
+
+            //_myTest.FirstUniqChar("coddoce");
+
+            //_myTest.IsSameTree(new TreeNode(0) { left = new TreeNode(2)}, new TreeNode(0) { left = new TreeNode(2) });
+
+            //_myTest.FindDisappearedNumbers(new int[] {3, 1, 1, 1});
+
+            //_myTest.ConvertToTitle("ZZZ");
+
+            //_myTest.IsAnagram("a","b");
+
+            //_myTest.ContainsDuplicate(new int[]{0,4,5,0,3,6});
+
+            //_myTest.LongestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth");
+
+            //_myTest.Intersect(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 });
+
+            //_myTest.FindComplement(5);
+
+            //_myTest.FindMaxConsecutiveOnes(new int[] {1, 0, 1, 1, 0, 1});
+
+            //_myTest.RomanToInt("DCCCXC");
+
+            //_myTest.MissingNumber(new [] { 0 });
+
+            //_myTest.ReverseList(new ListNode(1){next = new ListNode(2){next = new ListNode(3){next =new ListNode(4)}}});
+
+            //_myTest.DeleteNode(new ListNode(3){next = new ListNode(4)});
+
+            //_myTest.TwoSum(new []{2, 7, 11, 15}, 9 );
+
+            //_myTest.AddStrings("3876620623801494171","6529364523802684779");
+
+            _myTest.FindRelativeRanks(new [] {3,10,8,9,4});
+        }
+
+
+    }
+}
